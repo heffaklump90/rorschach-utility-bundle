e06b3069fc24a5f76624bd0501d69bb8c8c62b30
Björn Radon <bjorn.radon@agile-elevation.de>
2022-04-27 21:40:31 +0200

    debugging configuration for package

--- src/EventListener/TimestampListener.php
+++ src/EventListener/TimestampListener.php
@@ -3,17 +3,31 @@
 namespace Rorschach\UtilityBundle\EventListener;
 
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 
 class TimestampListener
 {
+    private ParameterBagInterface $parameterBag;
+    private string $timezone;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->parameterBag = $parameterBag;
+        $this->timezone = $this->parameterBag->get('utility.timezone');
+        if(!$this->timezone){
+            $this->timezone = 'Europe/Berlin';
+        }
+    }
+
+
     public function prePersist(LifecycleEventArgs $event)
     {
         $object = $event->getObject();
         if(method_exists($object, 'setCreatedAt')){
-            $object->setCreatedAt(new \DateTime('now', new \DateTimeZone('Europe/Berlin')));
+            $object->setCreatedAt(new \DateTime('now', new \DateTimeZone($this->timezone)));
         }if(method_exists($object, 'setUpdatedAt')){
-        $object->setUpdatedAt(new \DateTime('now', new \DateTimeZone('Europe/Berlin')));
+        $object->setUpdatedAt(new \DateTime('now', new \DateTimeZone($this->timezone)));
     }
     }
 
@@ -21,7 +35,7 @@ class TimestampListener
     {
         $object = $event->getObject();
         if(method_exists($object, 'setUpdatedAt')){
-            $object->setUpdatedAt(new \DateTime('now', new \DateTimeZone('Europe/Berlin')));
+            $object->setUpdatedAt(new \DateTime('now', new \DateTimeZone($this->timezone)));
         }
     }
 }
