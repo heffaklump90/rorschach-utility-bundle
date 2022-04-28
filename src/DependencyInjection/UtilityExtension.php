b8e172e45d22c930f59c10262208ecb224bb6b83
Björn Radon <bjorn.radon@agile-elevation.de>
2022-04-28 10:41:17 +0200

    debugging configuration for package

--- src/DependencyInjection/UtilityExtension.php
+++ src/DependencyInjection/UtilityExtension.php
@@ -12,7 +12,7 @@ class UtilityExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator('@UtilityBundle/Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
