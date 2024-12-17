483451c70d6a6ae69712c849a4c522cf1cf0a6e1
Bjoern Andersson <bjoern.andersson@agile-elevation.de>
2024-12-17 16:41:30 +0100

    version upgrade

---
+++ UtilityBundle.php
@@ -0,0 +1,37 @@
+<?php
+
+
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * UtilityBundle
+ */
+class UtilityBundle extends AbstractBundle
+{
+    public function configure(DefinitionConfigurator $definition):void
+    {
+        $definition->rootNode()
+            ->children()
+                ->arrayNode('timestamp')
+                    ->children()
+                        ->scalarNode('timezone')->defaultValue('Europe/Berlin')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('config/services.yaml');
+
+        $container->services()
+            ->get('timestamp.timezone')
+            ->arg(0, $config['timestamp']['timezone'])
+        ;
+    }
+
+}
