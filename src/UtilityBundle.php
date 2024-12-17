20398f2d1479f467da1005775a6acc99c3f1efaf
Bjoern Andersson <bjoern.andersson@agile-elevation.de>
2024-12-17 16:50:26 +0100

    version upgrade

--- src/UtilityBundle.php
+++ src/UtilityBundle.php
@@ -27,7 +27,7 @@ class UtilityBundle extends AbstractBundle
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $container->import('config/services.yaml');
+        $container->import('../config/services.yaml');
 
         $container->services()
             ->get('timestamp.timezone')
